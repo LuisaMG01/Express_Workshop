8b27a26785157074fbb82701a5d08f468749696f
LuisaMG01 <lmalvarez8@eafit.edu.co>
2024-05-13 15:52:37 -0500

    Images into the json database

--- src/config/cloudStorage.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  projectId: process.env.GOOGLE_PROJECT_ID,
-  clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  privateKey: process.env.GOOGLE_PRIVATE_KEY,
-  bucketName: process.env.GOOGLE_BUCKET_NAME,
-};
